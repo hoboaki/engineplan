50d748d5fa91a51a38d4eecc12ab7067d9f7188f
hoboaki <hoboaki@10106.net>
2017-09-18 16:10:17 +0900

    プラグインとアドオンの不足機能を追加。

--- EngineSrc/AdelEngine/AdelDevKit/PluginSystem/AddonSetupArg.cs
+++ EngineSrc/AdelEngine/AdelDevKit/PluginSystem/AddonSetupArg.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,5 +13,16 @@ namespace AdelDevKit.PluginSystem
     /// </summary>
     public class AddonSetupArg
     {
+        //------------------------------------------------------------------------------
+        /// <summary>
+        /// 環境情報。
+        /// </summary>
+        public EnvInfo EnvInfo { get; internal set; }
+
+        //------------------------------------------------------------------------------
+        /// <summary>
+        /// このアドオンが含まれているプラグインのルートディレクトリ。（*.aeplugin ディレクトリ）
+        /// </summary>
+        public DirectoryInfo PluginDir { get; internal set; }
     }
 }
