474d9b0d033a25470c698bac789e4a79c1614c74
hoboaki <hoboaki@10106.net>
2017-09-22 22:55:59 +0900

    自動コピー処理をビルド時イベントからはずす

--- EngineSrc/AdelBuildKitWin/DevKitProject/CopyToAdelDevProject.ps1
+++
@@ -1,6 +0,0 @@
-# 文字コード：UTF-8
-echo "Copy to AdelDevProject ..."
-$dstDir = "../DevelopResource/AdelDevProject/Plugin/AdelBuildKitWin.aeplugin/DevKitDll"
-Remove-Item $dstDir -Recurse
-Copy-Item ./bin/Release -destination $dstDir -recurse
-echo "Copy done."
