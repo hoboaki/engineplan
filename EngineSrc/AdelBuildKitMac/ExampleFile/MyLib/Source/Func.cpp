9bfeb3d23eaaf17bc883d6ef73ee52a9949488e0
hoboaki <hoboaki@10106.net>
2017-09-29 23:03:35 +0900

    サンプルプロジェクトを追加

---
+++ EngineSrc/AdelBuildKitMac/ExampleFile/MyLib/Source/Func.cpp
@@ -0,0 +1,14 @@
+//
+//  Func.cpp
+//  MyLib
+//
+//  Created by あっきー on 2017/09/29.
+//  Copyright © 2017年 あっきー. All rights reserved.
+//
+
+#include <cstdio>
+extern void MyFunc();
+void MyFunc()
+{
+    std::printf("MyFunc Called.\n");
+}
