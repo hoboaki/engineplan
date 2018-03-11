0cf52ca77324fd1f1495d915b932c0c59ca037c4
hoboaki <hoboaki@10106.net>
2018-03-11 23:38:18 +0900

    feat: xmain 先頭でダミーのprintfを出力。

--- EngineSrc/AdelEngine/AdelEngineNative/Source/ae/base/EntryPoint.cpp
+++ EngineSrc/AdelEngine/AdelEngineNative/Source/ae/base/EntryPoint.cpp
@@ -1,10 +1,13 @@
 // 文字コード：UTF-8
 #include <ae/base/EntryPoint.hpp>
 
+#include <ae/base/Console.hpp>
+
 //------------------------------------------------------------------------------
 int xmain(::ae::base::Application&)
 {
     // 何もせずにアプリケーションを終了するコード。
+    AE_BASE_COUT_LINE("This is xmain.");
     return 0;
 }
 
