215ea9c3b49371150641fec04c633b6e942a1f3d
hoboaki <hoboaki@10106.net>
2018-03-14 00:32:09 +0900

    feat: 外部ライブラリのサンプルコード。

--- EngineSrc/AdelBuildKitIos/ExampleFile/MyCocoaTouchApp/MyCocoaTouchApp/AppDelegate.m
+++ EngineSrc/AdelBuildKitIos/ExampleFile/MyCocoaTouchApp/MyCocoaTouchApp/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 
+extern void MyFunc();
+
 @interface AppDelegate ()
 
 @end
@@ -17,6 +19,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    MyFunc();
     return YES;
 }
 
