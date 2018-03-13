3955416c8c0de14fca0767b86f1897f0f9b10301
hoboaki <hoboaki@10106.net>
2018-03-14 00:50:50 +0900

    docs: コピーライト修正。

--- EngineSrc/AdelBuildKitIos/DevKitProject/Properties/AssemblyInfo.cs
+++ EngineSrc/AdelBuildKitIos/DevKitProject/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AdelBuildKitIos")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Adel Engine Developer")]
 [assembly: AssemblyProduct("AdelBuildKitIos")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 hoboaki")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
