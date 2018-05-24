98dcc37e55f6b8516addaad32c3b5e3192bebb1e
hoboaki <hoboaki@10106.net>
2018-05-24 23:48:19 +0900

    refactor: ビルドスクリプトのリファクタ。 - xcopy が mac で使えないためまだ不完全な状態。

--- EngineSrc/AdelBuildKitMac/UpdateEngineNativeProject.ps1
+++ EngineSrc/AdelBuildKitMac/UpdateEngineNativeProject.ps1
@@ -1,29 +1,13 @@
 # 文字コード：UTF-8
 $ErrorActionPreference = "Stop"
 
-# MSBuild定義
-$vswhereExe = "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe"
-if (!(Test-Path $vswhereExe)) {
-    echo "Not found vswhere."
-    exit 1
-}
-Invoke-Expression "& `"$vswhereExe`" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath" | Set-Variable msBuildExePath
-if (!($msBuildExePath)) {
-    echo "Not found MSBuild."
-    exit 1
-}
-$msBuildExe = join-path $msBuildExePath 'MSBuild\15.0\Bin\MSBuild.exe'
-function ExecMsBuild ($arg) {
-    $cmd = "`"$msBuildExe`"" + " " + $arg
-    Invoke-Expression "& `"$msBuildExe`" $arg"
-    if (!$?) {
-        echo "MsBuild Failed. $LASTEXITCODE"
-        exit 1
-    }
-}
+. "../DevelopSupport/PowerShell/Msbuild.ps1"
+
+# MSBuildを使用状態にする
+$msbuild = New-Object Msbuild
 
 # ビルドキットのビルド＆コピー
-ExecMsBuild(" /p:Configuration=Debug ./DevKitProject/AdelBuildKitMac.csproj")
+$msbuild.Execute(" /p:Configuration=Debug ./DevKitProject/AdelBuildKitMac.csproj")
 $srcDir = "./DevKitProject/bin/Debug"
 $dstDir = "./DevelopResource/AdelDevProject/Plugin/AdelBuildKitMac.aeplugin/DevKitDll"
 if (Test-Path $dstDir) {
@@ -32,8 +16,8 @@ if (Test-Path $dstDir) {
 Copy-Item $srcDir -destination $dstDir -recurse
 
 # AdelCommandのビルド
-ExecMsBuild(" /p:Configuration=Debug ../AdelEngineCore/AdelCommandMain/AdelCommandMain.csproj")
-ExecMsBuild(" /p:Configuration=Debug ../AdelEngineCore/AdelCommand/AdelCommand.csproj")
+$msbuild.Execute(" /p:Configuration=Debug ../AdelEngineCore/AdelCommandMain/AdelCommandMain.csproj")
+$msbuild.Execute(" /p:Configuration=Debug ../AdelEngineCore/AdelCommand/AdelCommand.csproj")
 
 # AdelCommandの実行
 & "../AdelEngineCore/AdelCommand/bin/Debug/AdelCommand.exe" -PrivateDevelopMode -ProjectDir "./DevelopResource/AdelDevProject" UpdateIdeProject
