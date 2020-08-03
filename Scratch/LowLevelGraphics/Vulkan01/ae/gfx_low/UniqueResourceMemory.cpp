96e47372ad54df1d8d5184e85829582b6c2befe1
hoboaki <hoboaki@10106.net>
2020-08-03 22:30:13 +0900

    feat: UniqueResourceMemory を追加。

---
+++ Scratch/LowLevelGraphics/Vulkan01/ae/gfx_low/UniqueResourceMemory.cpp
@@ -0,0 +1,43 @@
+// 文字コード：UTF-8
+#include <ae/gfx_low/UniqueResourceMemory.hpp>
+
+// includes
+#include <ae/base/PtrToRef.hpp>
+#include <ae/base/RuntimeAssert.hpp>
+#include <ae/gfx_low/Device.hpp>
+
+//------------------------------------------------------------------------------
+namespace ae {
+namespace gfx_low {
+
+//------------------------------------------------------------------------------
+void UniqueResourceMemory::Reset() {
+    if (memory_.IsValid()) {
+        device_->FreeResourceMemory(memory_);
+    }
+    device_.Reset();
+    memory_ = ResourceMemory();
+}
+
+//------------------------------------------------------------------------------
+void UniqueResourceMemory::Reset(Device* device, const ResourceMemoryAllocInfo& allocInfo) {
+    Reset();
+    device_.Reset(&base::PtrToRef(device));
+    memory_ = device_->AllocResourceMemory(allocInfo);
+}
+
+//------------------------------------------------------------------------------
+void UniqueResourceMemory::Reset(Device* device, const ResourceMemory& memory) {
+    Reset();
+    device_.Reset(&base::PtrToRef(device));
+    memory_ = memory;
+}
+
+//------------------------------------------------------------------------------
+UniqueResourceMemory::~UniqueResourceMemory() {
+    Reset();
+}
+
+} // namespace gfx_low
+} // namespace ae
+// EOF
---
+++ Scratch/LowLevelGraphics/Vulkan01/ae/gfx_low/UniqueResourceMemory.hpp
@@ -0,0 +1,61 @@
+// 文字コード：UTF-8
+#pragma once
+
+#include <ae/base/Noncopyable.hpp>
+#include <ae/base/Pointer.hpp>
+#include <ae/gfx_low/ResourceMemory.hpp>
+
+namespace ae {
+namespace gfx_low {
+class Device;
+class ResourceMemoryAllocInfo;
+} // namespace gfx_low
+} // namespace ae
+
+//------------------------------------------------------------------------------
+namespace ae {
+namespace gfx_low {
+
+/// リソースメモリをデストラクタで解放する　ResourceMemory
+/// 専用のスマートポインタのようなクラス。
+class UniqueResourceMemory : base::Noncopyable<UniqueResourceMemory> {
+public:
+    /// @name コンストラクタとデストラクタ
+    //@{
+    UniqueResourceMemory() { Reset(); }
+    UniqueResourceMemory(
+        Device* device, const ResourceMemoryAllocInfo& allocInfo)
+    : UniqueResourceMemory() {
+        Reset(device, allocInfo);
+    }
+    UniqueResourceMemory(Device* device, const ResourceMemory& memory)
+    : UniqueResourceMemory() {
+        Reset(device, memory);
+    }
+    ~UniqueResourceMemory();
+    //@}
+
+    /// @name リセット
+    //@{
+    void Reset();
+    void Reset(Device* device, const ResourceMemoryAllocInfo& allocInfo);
+    void Reset(Device* device, const ResourceMemory& memory);
+    //@}
+
+    /// @name 演算子オーバーロード
+    //@{
+    /// 参照演算子。
+    const ResourceMemory& operator*() const { return memory_; }
+
+    /// アドレス演算子。
+    const ResourceMemory* operator->() const { return &memory_; }
+    //@}
+
+private:
+    base::Pointer<Device> device_;
+    ResourceMemory memory_;
+};
+
+} // namespace gfx_low
+} // namespace ae
+// EOF
