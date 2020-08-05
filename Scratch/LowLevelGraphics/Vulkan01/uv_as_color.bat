216133e79d407c4ec1a624b2e8f6b5aab5faa909
hoboaki <hoboaki@10106.net>
2020-08-06 00:00:58 +0900

    feat: uv を色で表示するシェーダーを追加。

---
+++ Scratch/LowLevelGraphics/Vulkan01/uv_as_color.bat
@@ -0,0 +1,4 @@
+:: 文字コード：SJIS
+%VULKAN_SDK%\Bin\glslangValidator.exe -V -x -o ./uv_as_color.vert.inc ./uv_as_color.vert
+%VULKAN_SDK%\Bin\glslangValidator.exe -V -x -o ./uv_as_color.frag.inc ./uv_as_color.frag
+:: EOF
