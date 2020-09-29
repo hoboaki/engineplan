880b64a93f84ad1c2db6c78eb9978598ba4a3020
hoboaki <hoboaki@10106.net>
2020-09-29 23:43:43 +0900

    feat: シェーダーコード追加。

---
+++ Scratch/LowLevelGraphics/Vulkan01/textured_cube.bat
@@ -0,0 +1,4 @@
+:: 文字コード：SJIS
+%VULKAN_SDK%\Bin\glslangValidator.exe -V -x -o ./textured_cube.vert.inc ./textured_cube.vert
+%VULKAN_SDK%\Bin\glslangValidator.exe -V -x -o ./textured_cube.frag.inc ./textured_cube.frag
+:: EOF
