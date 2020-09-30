a8ad3cc727d8769085a180ef25c6e2d0dce4ac79
hoboaki <hoboaki@10106.net>
2020-09-30 23:26:23 +0900

    feat: ImageSubresourceRange の追加。

---
+++ Scratch/LowLevelGraphics/Vulkan01/ae/gfx_low/ImageSubresourceRange.cpp
@@ -0,0 +1,30 @@
+// 文字コード：UTF-8
+#include <ae/gfx_low/ImageSubresourceRange.hpp>
+
+// includes
+#include <ae/base/RuntimeAssert.hpp>
+#include <ae/gfx_low/ImageSubresourceLocation.hpp>
+
+//------------------------------------------------------------------------------
+namespace ae {
+namespace gfx_low {
+
+//------------------------------------------------------------------------------
+ImageSubresourceRange& ImageSubresourceRange::SetMipLevels(
+    const int levels) {
+    AE_BASE_ASSERT_LESS_EQUALS(1, levels);
+    mipLevels_ = levels;
+    return *this;
+}
+
+//------------------------------------------------------------------------------
+ImageSubresourceRange& ImageSubresourceRange::SetArrayLength(
+    const int arrayLength) {
+    AE_BASE_ASSERT_LESS_EQUALS(0, arrayLength);
+    arrayLength_ = arrayLength;
+    return *this;
+}
+
+} // namespace gfx_low
+} // namespace ae
+// EOF
---
+++ Scratch/LowLevelGraphics/Vulkan01/ae/gfx_low/ImageSubresourceRange.hpp
@@ -0,0 +1,55 @@
+// 文字コード：UTF-8
+#pragma once
+
+#include <ae/gfx_low/ImageSubresourceLocation.hpp>
+
+//------------------------------------------------------------------------------
+namespace ae {
+namespace gfx_low {
+
+/// ImageResource の抱える ImageSubresource の範囲を示すクラス。
+/// @details
+/// ・キューブイメージの ArrayLength の指定について
+/// 対象となる ImageResource の種類が ImageResourceKind::ImageCube で
+/// かつ BaseLocation().FaceIndex() が 1 以上の場合、
+/// ArrayLength() は 1 以外の値を指定することができません。
+class ImageSubresourceRange {
+public:
+    /// @name プロパティ
+    //@{
+    /// 起点となる位置。（初期値：デフォルトコンストラクタの値）
+    ImageSubresourceLocation BaseLocation() const { return baseLocation_; }
+
+    /// BaseLocation() の設定。
+    ImageSubresourceRange& SetBaseLocation(
+        const ImageSubresourceLocation& location) {
+        baseLocation_ = location;
+        return *this;
+    }
+
+    /// Mip レベル数。（初期値：1）
+    int MipLevels() const { return mipLevels_; }
+
+    /// MipLevel() の設定。
+    /// @param level 0以上。
+    ImageSubresourceRange& SetMipLevels(int levels);
+
+    /// 配列長。（初期値：1）
+    int ArrayLength() const { return arrayLength_; }
+
+    /// ArrayLength() の設定。
+    /// @param length 1以上。
+    /// @details 対象の ImageResource
+    /// がキューブイメージの場合、指定に制限があります。詳しくは本クラスの注釈を参照してください。
+    ImageSubresourceRange& SetArrayLength(int length);
+    //@}
+
+private:
+    ImageSubresourceLocation baseLocation_;
+    int mipLevels_ = 0;
+    int arrayLength_ = 0;
+};
+
+} // namespace gfx_low
+} // namespace ae
+// EOF
