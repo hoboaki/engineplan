c36a9319c14dee3c7b258460791e804721748d89
hoboaki <hoboaki@10106.net>
2021-09-01 21:49:15 +0900

    feat: サンプル06追加。

---
+++ Scratch/LowLevelGraphics/VulkanSample/AeVulkan06/ConvertShader.bat
@@ -0,0 +1,5 @@
+:: 文字コード：SJIS
+%VULKAN_SDK%\Bin\glslangValidator.exe -V -x -o ./Shader.comp.inc ./Shader.comp
+%VULKAN_SDK%\Bin\glslangValidator.exe -V -x -o ./Shader.vert.inc ./Shader.vert
+%VULKAN_SDK%\Bin\glslangValidator.exe -V -x -o ./Shader.frag.inc ./Shader.frag
+:: EOF
