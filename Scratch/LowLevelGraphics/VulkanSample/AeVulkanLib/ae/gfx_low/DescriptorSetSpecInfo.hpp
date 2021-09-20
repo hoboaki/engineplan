7ca0a4e764cd5082c2543b841f46ac32f020cfc6
hoboaki <hoboaki@10106.net>
2021-09-21 00:15:35 +0900

    feat: DirectConstantInfo を DescriptorSetSpecInfo に追加。

--- Scratch/LowLevelGraphics/VulkanSample/AeVulkanLib/ae/gfx_low/DescriptorSetSpecInfo.hpp
+++ Scratch/LowLevelGraphics/VulkanSample/AeVulkanLib/ae/gfx_low/DescriptorSetSpecInfo.hpp
@@ -7,6 +7,7 @@
 
 namespace ae {
 namespace gfx_low {
+class DirectConstantInfo;
 class ShaderBindingInfo;
 ;
 } // namespace gfx_low
@@ -22,9 +23,20 @@ namespace gfx_low {
 /// BindingInfos() のインデックスをバインド番号として扱います。
 /// 各プラットフォーム毎のバインド番号の仕様・制約は以下の通りです。
 ///
-/// - VK-GLSL と MSL において buffer(storage) のシェーダーコード上バインド番号は buffer(uniform) の最大番号 + 1 からスタート。
-/// - VK-GLSL と MSL において image(write) のシェーダーコード上バインド番号は image(read)の最大番号 + 1 からスタート。
-/// - HLSL において image(write) のシェーダーコード上バインド番号は buffer(storage)の最大番号 + 1 からスタート。
+/// **VK-GLSL**
+/// - buffer(storage) のシェーダーコード上バインド番号は buffer(uniform) の最大番号 + 1 からスタート。
+/// - image(write) のシェーダーコード上バインド番号は image(read)の最大番号 + 1 からスタート。
+/// 
+/// **MSL**
+/// - 通常 buffer(storage) のシェーダーコード上バインド番号は buffer(uniform) の最大番号 + 1 からスタート。
+///   ただし、ダイレクトコンスタントを使う場合は buffer(uniform) の最大番号 + 1 が
+///   ダイレクトコンスタントに割り当てられる。その場合は buffer(storage) の
+///   シェーダーコード上バインド番号は buffer(uniform) の最大番号 + 2 からスタートさせること。
+/// - image(write) のシェーダーコード上バインド番号は image(read)の最大番号 + 1 からスタート。
+/// 
+/// **HLSL**
+/// - ダイレクトコンスタントのバインド番号は buffer(uniform) の最大番号 + 1 を割り当てる。
+/// - image(write) のシェーダーコード上バインド番号は buffer(storage)の最大番号 + 1 からスタート。
 ///
 /// 例えば VK-GLSL 環境において UniformBuffer が3つ、StorageBuffer を2つ使う場合、
 /// シェーダーコード上では buffer(uniform) の番号を 0,1,2 に割り当て、
@@ -35,30 +47,61 @@ namespace gfx_low {
 ///
 /// また、VK-GLSL 環境では DescriptorSet 番号を Buffer, Image, Sampler の順番で DescriptorSet 番号を 0 から順番に割り当ててください。
 /// Buffer を使わず Image ＆ Sampler だけ使うといった場合は Image に set=0、Sampler に set=1 を割り当ててください。
+/// 
+/// 
+/// ダイレクトコンスタントは１シェーダーステージにつき１つまでしか割り当てることはできません。
+/// またダイレクトコンスタントの扱えるサイズは実行環境によって異なります。
+/// 推奨としては全ダイレクトコンスタントの合計サイズが 128 以下とします。
 class DescriptorSetSpecInfo {
 public:
     /// @name プロパティ
     //@{
     /// 指定のデスクリプタ種類のバインド情報の数。（初期値：0）
     int BindingInfoCount(DescriptorKind kind) const {
-        return infos_[kind].infoCount;
+        return bindingInfos_[kind].infoCount;
     }
 
     /// 指定のデスクリプタ種類のバインド情報郡。（初期値：nullptr）
     const ShaderBindingInfo* BindingInfos(DescriptorKind kind) const {
-        return infos_[kind].infos.Get();
+        return bindingInfos_[kind].infos.Get();
     }
 
     /// BindingInfoCount() BindingInfos() の設定。
     /// @param count 0 以上。
-    /// @infos count == 0 のときは nullptr を指定。それ以外のときは配列のポインタを指定。
+    /// @param infos count == 0 のときは nullptr を指定。それ以外のときは配列のポインタを指定。
+    /// バインドに関する制約についてはクラスコメントを参照してください。
     DescriptorSetSpecInfo& SetBindingInfos(
         DescriptorKind kind,
         int count,
         const ShaderBindingInfo* infosPtr = nullptr);
 
     /// 指定のデスクリプタ種類の総バインド数を計算して取得。（初期値：0）
+    /// @details
+    /// ここにダイレクトコンスタントデータは含まれていません。
     int TotalBindingCount(DescriptorKind kind) const;
+
+    /// ダイレクトコンスタント情報の数。（初期値：0）
+    int DirectConstantInfoCount() const { return directConstantInfoCount_; }
+
+    /// ダイレクトコンスタントの情報群。（初期値：nullptr）
+    const DirectConstantInfo* DirectConstantInfos() const {
+        return directConstantInfos_.Get();
+    }
+        
+    /// DirectConstantCount() DirectConstantInfos() の設定。
+    /// @param count 0 以上。
+    /// @param infos count == 0 のときは nullptr を指定。それ以外のときは配列のポインタを指定。
+    /// @details 
+    /// ダイレクトコンスタントに関する制約についてはクラスコメントを参照してください。
+    DescriptorSetSpecInfo& SetDirectConstantInfos(
+        int count,
+        const DirectConstantInfo* infosPtr = nullptr);
+    //@}
+
+    /// @name 内部処理用機能
+    //@{
+    /// 内容が問題ないか確認する。
+    void Validate() const;
     //@}
 
 private:
@@ -68,7 +111,9 @@ private:
         base::Pointer<const ShaderBindingInfo> infos;
     };
 
-    base::EnumKeyArray<DescriptorKind, DescriptorInfo> infos_;
+    int directConstantInfoCount_ = 0;
+    base::EnumKeyArray<DescriptorKind, DescriptorInfo> bindingInfos_;
+    base::Pointer<const DirectConstantInfo> directConstantInfos_;
 };
 
 } // namespace gfx_low
---
+++ Scratch/LowLevelGraphics/VulkanSample/AeVulkanLib/ae/gfx_low/DirectConstantInfo.hpp
@@ -0,0 +1,42 @@
+// 文字コード：UTF-8
+#pragma once
+
+#include <ae/gfx_low/ShaderBindingStageBitSet.hpp>
+
+//------------------------------------------------------------------------------
+namespace ae {
+namespace gfx_low {
+
+/// ダイレクトコンスタントに関する情報。
+class DirectConstantInfo {
+public:
+    /// @name プロパティ
+    //@{
+    /// 参照するシェーダーステージ郡。（初期値：AllOff()）
+    ShaderBindingStageBitSet Stages() const { return stages_; }
+
+    /// Stages() の設定。
+    DirectConstantInfo& SetStages(const ShaderBindingStageBitSet& stages) {
+        stages_ = stages;
+        return *this;
+    }
+
+    /// データブロックサイズ。（初期値：0）
+    size_t Size() const { return size_; }
+
+    /// Size() の設定。
+    DirectConstantInfo& SetSize(size_t size) {
+        size_ = size;
+        return *this;
+    }
+    //@}
+
+private:
+    ShaderBindingStageBitSet stages_;
+    size_t bindingIndex_ = 0;
+    size_t size_ = 1;
+};
+
+} // namespace gfx_low
+} // namespace ae
+// EOF
