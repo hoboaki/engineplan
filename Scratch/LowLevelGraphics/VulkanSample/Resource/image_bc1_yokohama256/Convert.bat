3bd1cff6a2f23ccc86e695713ba903d91a0f1e14
hoboaki <hoboaki@10106.net>
2022-02-02 23:41:38 +0900

    feat: ヘッダ版 Image を追加。

--- Scratch/LowLevelGraphics/VulkanSample/Resource/image_bc1_yokohama256/Convert.bat
+++ Scratch/LowLevelGraphics/VulkanSample/Resource/image_bc1_yokohama256/Convert.bat
@@ -1,4 +1,4 @@
 :: 文字コード：SJIS
-C:\app\DirectXTex\bin\texconv.exe -ft dds -f BC1_UNORM -y .\NegX.png
-
+C:\app\DirectXTex\bin\texconv.exe -ft dds -f BC1_UNORM -y .\Image.png
+..\..\Tool\BinaryToHeader\bin\Debug\netcoreapp3.1\BinaryToHeader.exe .\Image.dds
 :: EOF
